71f4e4d0b7a7e8be0394aedca7451d0e8f1d3d18
Vladyslav Burdeniuk <burdenyuchok@gmail.com>
2024-08-04 16:01:55 -0700

    Add UserInfo component

---
+++ src/app/_components/UserInfo.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+
+import Image from "next/image";
+
+const LoadingState = () => {
+  return (
+    <div className="flex w-full gap-4 items-center justify-start animate-pulse opacity-50">
+      <div className="inline-block bg-slate-100 h-20 w-20 rounded-full" />
+      <div className="bg-slate-100 w-48 h-4 rounded-md" />
+    </div>
+  );
+};
+
+export const UserInfo = () => {
+  const { user, isLoaded } = useUser();
+
+  if (!isLoaded) return <LoadingState />;
+
+  if (!user) return null;
+
+  return (
+    <div className="flex w-full gap-4 items-center justify-start">
+      <Image
+        alt={`${user.fullName}'`}
+        className="inline-block h-20 w-20 rounded-full"
+        height={80}
+        src={`${user.imageUrl}`}
+        width={80}
+      />
+      <span>{user.fullName}</span>
+    </div>
+  );
+};
